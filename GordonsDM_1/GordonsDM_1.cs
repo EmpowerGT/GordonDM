682f3a250e422c1acae12513e47525c57e732656
gordon.twyford <gordon.twyford@NTT-9QJSFB3>
2023-12-05 14:17:20 +0100

    Add project files.

---
+++ GordonsDM_1/GordonsDM_1.cs
@@ -0,0 +1,24 @@
+namespace GordonsDM_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+    using Skyline.DataMiner.Net.ReportsAndDashboards;
+
+    /// <summary>
+    /// Represents a DataMiner Automation script.
+    /// </summary>
+    public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			engine.GenerateInformation("HelloWorld");
+		}
+	}
+}
